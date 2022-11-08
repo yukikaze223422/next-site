e851cf0ff54e8d7c4cf4ca233191eda2ecd8bb6c
takagi <takagi09245@gmail.com>
2022-11-09 00:41:21 +0900

    公開準備

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import "../styles/mysite.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/components/content.js
@@ -0,0 +1,9 @@
+export default function Content({ children }) {
+  return (
+    <>
+      <div class="container">{children}</div>
+      <hr />
+      <footer>©️ 2021 ともすた</footer>
+    </>
+  );
+}
